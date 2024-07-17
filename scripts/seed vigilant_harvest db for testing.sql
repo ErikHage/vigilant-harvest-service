ccc025edff1caf1e2e00588fe4b8abad6a7d6451
Erik Hage <ehage4@gmail.com>
2024-07-17 00:01:51 -0400

    add seed script for test data

---
+++ scripts/seed vigilant_harvest db for testing.sql
@@ -0,0 +1,7 @@
+USE vigilant_harvest;
+
+INSERT INTO plants (plant_id, friendly_name, species, genus, family) 
+VALUES ('8191b92b-12b8-435f-a929-efd495c6ee1b', 'Big Red Cherry Tomato', 'Lycopersicum', 'Solanum', 'Solanaceae'),
+       ('8a6d493f-0f58-4e94-b3ab-f80d4cb0f57e', 'California Wonder Bell Pepper', 'Annuum', 'Capsicum', 'Solanaceae'),
+       ('a63ceba6-858d-43a0-b0bf-819637bf36d0', 'Fordhook Zuchini', 'Pepo', 'Cucurbita', 'Cucurbitaceae'),
+       ('b94b01b1-35ef-47e7-81a8-7045d80fe686', 'Brandywine Tomato', 'Lycopersicum', 'Solanum', 'Solanaceae');
