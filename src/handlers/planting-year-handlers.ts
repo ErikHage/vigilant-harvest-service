6822a71a666675c89d817beb8a274c89bfee373a
Erik Hage <ehage4@gmail.com>
2024-12-07 01:07:23 -0500

    add planting year handlers

---
+++ src/handlers/planting-year-handlers.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+
+import plantingYearSerializers from '../serializers/planting-years';
+import plantingYearsService from '../services/planting-years/planting-years-service';
+
+async function insertPlantingYear(request: Request, response: Response) {
+  const plantingRequest = plantingYearSerializers.fromRequest(request);
+
+  await plantingYearsService.insertPlantingYear(plantingRequest);
+
+  response.sendStatus(201);
+}
+
+async function getPlantingYears(request: Request, response: Response) {
+
+  const plantingYears = await plantingYearsService.getPlantingYears();
+
+  const plantingYearsResponse = plantingYears.map(plantingYearSerializers.toResponse);
+
+  response
+    .status(200)
+    .send(plantingYearsResponse);
+}
+
+export default {
+  insertPlantingYear,
+  getPlantingYears,
+}
