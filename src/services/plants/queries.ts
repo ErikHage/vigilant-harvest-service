6c8d526a70a92127782e6e7d1b62d4ba22475b0f
Erik Hage <ehage4@gmail.com>
2025-01-13 19:10:21 -0500

    add remaining fields to plant upsert query

--- src/services/plants/queries.ts
+++ src/services/plants/queries.ts
@@ -27,7 +27,6 @@ harvest_instructions,
 date_created,
 date_modified`;
 
-// TODO add rest of fields to ON UPDATE clause
 const upsertPlant: string = `
   INSERT into plants (plant_id, category, seed_source, tags, plant_description, friendly_name, family, genus, species,
   indoor_sowing, direct_sowing, germination_days_range, germination_temp_range, planting_depth_inches,
@@ -46,7 +45,19 @@ const upsertPlant: string = `
          indoor_sowing =          VALUES(indoor_sowing),
          direct_sowing =          VALUES(direct_sowing),
          germination_days_range = VALUES(germination_days_range),
-         germination_temp_range = VALUES(germination_temp_range)
+         germination_temp_range = VALUES(germination_temp_range),
+         planting_depth_inches =  VALUES(planting_depth_inches),
+         plant_spacing_inches =   VALUES(plant_spacing_inches),
+         row_spacing_inches =     VALUES(row_spacing_inches),
+         planting_instructions =  VALUES(planting_instructions),
+         required_sun =           VALUES(required_sun),
+         days_to_maturity =       VALUES(days_to_maturity),
+         is_climbing =            VALUES(is_climbing),
+         climbing_height_feet =   VALUES(climbing_height_feet),
+         plant_size =             VALUES(plant_size),
+         fruit_size =             VALUES(fruit_size),
+         shelf_stability =        VALUES(shelf_stability),
+         harvest_instructions =   VALUES(harvest_instructions)
 `;
 
 const getById: string = `SELECT ${allPlantFields} FROM plants WHERE plant_id = ?`;
