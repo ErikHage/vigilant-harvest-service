08257efb0f1734943888af9b9ecf6e2065fb96d8
Erik Hage <ehage4@gmail.com>
2025-02-23 15:59:40 -0500

    add delete planting action

---
+++ src/services/plantings/actions/delete-action.ts
@@ -0,0 +1,30 @@
+import { PlantingAction } from './planting-action';
+import { PerformActionRequest, Planting } from '../types';
+import datasource from '../plantings-mysql-datasource';
+import { FeralError } from '../../../errors';
+
+export class DeleteAction implements PlantingAction {
+  async performAction(plantingActionRequest: PerformActionRequest): Promise<Planting> {
+    try {
+      await datasource.deletePlantingById(plantingActionRequest.plantingId);
+    } catch (err) {
+      throw new DeleteActionError().withDebugParams({ plantingActionRequest, });
+    }
+
+    // to fit the pattern, doesn't really matter
+    return {
+      plantingId: '-1',
+      plantId: '-1',
+      plantingYear: 1,
+      name: 'Deleted',
+      currentStatus: 'DELETED',
+      notes: [],
+    };
+  }
+}
+
+class DeleteActionError extends FeralError {
+  constructor() {
+    super('Error performing DELETE action for planting');
+  }
+}
--- src/services/plantings/actions/planting-action-factory.ts
+++ src/services/plantings/actions/planting-action-factory.ts
@@ -3,10 +3,12 @@ import { StartAction } from './start-action';
 import { PlantAction } from './plant-action';
 import { RetireAction } from './retire-action';
 import { FeralError } from '../../../errors';
+import { DeleteAction } from './delete-action';
 import constants from '../../../util/constants';
 
 const actionMap: Map<string, PlantingAction> = new Map<string, PlantingAction>();
 
+actionMap.set(constants.plantings.actionTypes.delete, new DeleteAction());
 actionMap.set(constants.plantings.actionTypes.start, new StartAction());
 actionMap.set(constants.plantings.actionTypes.plant, new PlantAction());
 actionMap.set(constants.plantings.actionTypes.retire, new RetireAction());
