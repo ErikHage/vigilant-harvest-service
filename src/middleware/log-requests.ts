28e069efebbc4adc134a8a11126d046be0f2e9ac
Erik Hage <ehage4@gmail.com>
2025-09-07 22:45:45 -0400

    also log request query params in DEV

--- src/middleware/log-requests.ts
+++ src/middleware/log-requests.ts
@@ -6,6 +6,7 @@ import { Request, Response, NextFunction } from 'express';
 export default (request: Request, response: Response, next: NextFunction) => {
   if (process.env.DEPLOYMENT_ENVIRONMENT === 'dev') {
     console.log('Request: ' + request.method + ': ' + request.baseUrl + request.path);
+    console.log('  query:', request.query);
     console.log(' params:', request.params);
     console.log('   body:', request.body);
   }
